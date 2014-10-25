6eb1bc63df6cd0faa12295dc6a3d5a03f88b1459
Chris Molanus <chris.molanus@incentro.com>
2014-10-25 17:58:40 +0200

    Changed the example function to one that does what they asked for

--- cachematrix.R
+++ cachematrix.R
@@ -1,48 +1,54 @@
 ## Return a object (so a list) that holds a matrix (x), default is am empty matrix
 # and that has a set function "set" that stores a new matrix
 # and that has a get function "get" that returns the stored matrix
-# and that has a set function "setmean" that stores a given mean
-# and that has a get function "getmean" that returns the stored mean
+# and that has a set function "setInverse" that stores a given mean
+# and that has a get function "getInverse" that returns the stored mean
 #
 # The object resets the value given by "getmean" to "NULL" everytime "set" is called
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+  # create a variable ot store the inverted matrix
+  i <- NULL
+  
+  # A function to set a new matrix
   set <- function(y) {
     x <<- y
     
     #Clear the stored value of m (from setmean)
-    m <<- NULL
+    i <<- NULL
   }
+  
+  # a function to return teh stored matrix
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  
+  setInverse <- function(solve) i <<- solve
+  getInverse <- function() i
+  
+  #Return object with functions
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Return the mean of x, x must be a object created by "makeCacheMatrix"
+## Return the inverse matrix of x, x must be a object created by "makeCacheMatrix"
 cacheSolve <- function(x, ...) {
-  m <- x$getmean()
+  i <- x$getmean()
   
-  # Check if there is a stored mean
-  if(!is.null(m)) {
+  # Check if there is a stored inverse matrix
+  if(!is.null(i)) {
     message("getting cached data")
-    return(m)
+    # Since it's cached, just return the cashed version
+    return(i)
   }
   
   # There wasn't a stored mean so calulate it and store it
   
-  # get the matrix
-  data <- x$get()
+  # calculate the inverse matrix
+  i <- solve(x$get(), ...)
   
-  # calculate the mean
-  m <- mean(data, ...)
+  # store the inverse matrix in the object
+  x$setmean(i)
   
-  # store the mean in the object
-  x$setmean(m)
-  
-  # return the calculated mean
-  m
+  # return the calculated inverse matrix
+  i
 }
